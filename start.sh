5f47095b6f65a4130276dfa5936999052589622f
Danss0 <rizkydante0@gmail.com>
2025-08-15 09:33:44 +0700

    update

--- start.sh
+++ start.sh
@@ -1,18 +1,8 @@
+#!/bin/bash
 set -eu
 
+# Pastikan log langsung tampil
 export PYTHONUNBUFFERED=true
 
-VIRTUALENV=.data/venv
-
-if [ ! -d $VIRTUALENV ]; then
-  python3 -m venv $VIRTUALENV
-fi
-
-if [ ! -f $VIRTUALENV/bin/pip ]; then
-  curl --silent --show-error --retry 5 https://bootstrap.pypa.io/get-pip.py | $VIRTUALENV/bin/python
-fi
-
-$VIRTUALENV/bin/pip install -r requirements.txt
-
-$VIRTUALENV/bin/python3 app.py
-Footer
+# Jalankan dengan Gunicorn
+gunicorn app:app --bind 0.0.0.0:$PORT --workers 4
